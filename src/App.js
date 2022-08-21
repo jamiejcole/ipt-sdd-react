fe4bc0afadda4076dbc09b457e94aa0d192947c1
Jamie Cole <jamiejcole@icloud.com>
2022-08-21 21:18:43 +1000

    made react work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+//import NameForm from './components/form';
+import RadioForm from './components/radioForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          SDD & IPT Question Finder
+        <h1 className="text-stone-800 text-3xl font-bold underline pt-10">
+          SDD IPT Question Finder
         </h1>
       </header>
+
+      <RadioForm t1="SDD" d1="Software Design & Development" t2="IPT" d2="Information Processes & Technology" />
+
     </div>
   );
 }
---
+++ src/components/form.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            {this.props.title}: 
+            <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+
+export default NameForm;
