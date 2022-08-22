1b7eecc52c27fe0c2439c5b645893746998b4535
Jamie Cole <jamiejcole@icloud.com>
2022-08-22 17:36:21 +1000

    added random year checkbox

--- src/App.js
+++ src/App.js
@@ -7,22 +7,27 @@ import SearchForm from './components/searchForm';
 const App = () => {
   const [course, setCourse] = useState();
   const [questionsOrSolutions, setQuestionsOrSolutions] = useState();
+  const [year, setYear] = useState();
 
   const buttonManager = (formID, selection) => {
     console.log(formID, selection);
   }
 
+  const searchManager = (searchID, text) => {
+    console.log(searchID, text);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-stone-800 text-3xl font-bold underline pt-10">
-          SDD IPT Question Finder
+        <h1 className="text-stone-800 text-3xl font-black underline pt-10">
+          SDD & IPT Question Finder
         </h1>
       </header>
 
       <RadioForm buttonManager={buttonManager} title="Which course?" t1="SDD" d1="Software Design & Development" t2="IPT" d2="Information Processes & Technology" />
       <RadioForm buttonManager={buttonManager} title="What content?" t1="Questions" d1=" " t2="Solutions" d2=" " />
-      <SearchForm title="Which year?" />
+      <SearchForm searchManager={searchManager} title="Which year?" />
     </div>
   );
 }
