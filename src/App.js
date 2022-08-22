e58dcba91afd6f16c33ad4bf945a935612ef6900
Jamie Cole <jamiejcole@icloud.com>
2022-08-23 00:05:26 +1000

    added react helmet for title changing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, {useState} from 'react';
-import { findRenderedComponentWithType } from 'react-dom/test-utils';
+import { Helmet } from 'react-helmet';
 import './App.css';
 import RadioForm from './components/radioForm';
 import SearchForm from './components/searchForm';
@@ -45,28 +45,30 @@ const App = () => {
     return url
   }
 
-
   return (
-    <div className="flex flex-col items-center justify-center text-center">
-      <header className="App-header">
-        <h1 className="text-stone-800 text-3xl font-black underline pt-10">
-          SDD & IPT Question Finder
-        </h1>
-      </header>
+    <>
+      <Helmet><title>HSC Questions</title></Helmet>
+      <div className="flex flex-col items-center justify-center text-center">
+        <header className="App-header">
+          <h1 className="text-stone-800 text-3xl font-black underline pt-10">
+            SDD & IPT Question Finder
+          </h1>
+        </header>
 
-      <RadioForm buttonManager={buttonManager} title="Which course?" t1="SDD" d1="Software Design & Development" t2="IPT" d2="Information Processes & Technology" />
-      <RadioForm buttonManager={buttonManager} title="What content?" t1="Questions" d1=" " t2="Solutions" d2=" " />
-      
-      <div className="w-fit">
-        <SearchForm submitForm={submitForm} randManager={randManager} searchManager={searchManager} title="Which year?" placeholder="2020, 2021, etc" checkboxVisible={true} />
-        <SearchForm submitForm={submitForm} searchManager={searchManager} title="(Optional) Enter question number:" placeholder="23, 31, etc" checkboxVisible={false} submitVisible={true}/>
+        <RadioForm buttonManager={buttonManager} title="Which course?" t1="SDD" d1="Software Design & Development" t2="IPT" d2="Information Processes & Technology" />
+        <RadioForm buttonManager={buttonManager} title="What content?" t1="Questions" d1=" " t2="Solutions" d2=" " />
+        
+        <div className="w-fit">
+          <SearchForm submitForm={submitForm} randManager={randManager} searchManager={searchManager} title="Which year?" placeholder="2020, 2021, etc" checkboxVisible={true} />
+          <SearchForm submitForm={submitForm} searchManager={searchManager} title="(Optional) Enter question number:" placeholder="23, 31, etc" checkboxVisible={false} submitVisible={true}/>
+        </div>
+
+        <div className={"pt-10 flex items-center flex-row"}>
+          <button onClick={submitForm} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-lg w-full sm:w-auto px-8 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </div>
+
       </div>
-
-      <div className={"pt-10 flex items-center flex-row"}>
-        <button onClick={submitForm} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-lg w-full sm:w-auto px-8 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-      </div>
-
-    </div>
+    </>
   );
 }
 
