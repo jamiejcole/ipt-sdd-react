c76f0486e379366f876bb7e4fe42c89efe83eefa
Jamie Cole <jamiejcole@icloud.com>
2022-08-22 22:49:21 +1000

    added submit button

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import RadioForm from './components/radioForm';
 import SearchForm from './components/searchForm';
 
 const App = () => {
-  const [course, setCourse] = useState();
-  const [questionsOrSolutions, setQuestionsOrSolutions] = useState();
-  const [year, setYear] = useState();
+  const [course, setCourse] = useState("SDD");
+  const [content, setContent] = useState("Questions");
+  const [year, setYear] = useState("2021");
+  const [quesNum, setQuesNum] = useState();
 
   const buttonManager = (formID, selection) => {
-    console.log(formID, selection);
+    //console.log(formID, selection);
+    if (formID === "Which course?") setCourse(selection);
+    else if (formID === "What content?") setContent(selection);
   }
 
   const searchManager = (searchID, text) => {
-    console.log(searchID, text);
+    //console.log(searchID, text);
+    if (searchID === "Which year?") setYear(text);
+    else if (searchID === "(Optional) Enter question number:") setQuesNum(text);
   }
 
-  const questionManager = (questionID, text) => {
-    console.log(questionID, text);
+  const submitForm = () => {
+    console.log(course, content, year, quesNum);
   }
 
 
@@ -35,7 +40,11 @@ const App = () => {
       
       <div className="w-fit">
         <SearchForm searchManager={searchManager} title="Which year?" placeholder="2020, 2021, etc" checkboxVisible={true} />
-        <SearchForm questionManager={questionManager} title="(Optional) Enter question number:" placeholder="23, 31, etc" checkboxVisible={false} />
+        <SearchForm searchManager={searchManager} title="(Optional) Enter question number:" placeholder="23, 31, etc" checkboxVisible={false} submitVisible={true}/>
+      </div>
+
+      <div className={"pt-10 flex items-center flex-row"}>
+        <button onClick={submitForm} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-lg w-full sm:w-auto px-8 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </div>
 
     </div>
--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -12,24 +12,19 @@ const SearchForm = (props) => {
 
     return (
         <div className="grid place-items-center">
-            <form>   
-                <h3 className="pt-8 mb-5 text-xl font-bold text-gray-900">{props.title}</h3>
-                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-                <div className="relative flex flex-row gap-6 items-center justify-center">
-                    <div className="flex flex-row items-center justify-center gap-1 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
-                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                        <input value={textInput} disabled={isDisabled} onChange={(event) => {props.searchManager(props.title, event.target.value); setTextInput(event.target.value)}} type="number" id="default-search" className="w-2/3 block p-4 pl-10 w-full text-sm text-gray-900" placeholder={props.placeholder}>
-                            {/* this is fucky, get Big. T to fix when he is not fucky */}
-                        </input>
-                    </div>
-                    
-                    <div className={"flex items-center flex-row " + checkboxVisible}>
-                        <input id="checkbox" onChange={(event) => {setIsDisabled(event.target.checked); setTextInput('')}} type="checkbox" value="" className="w-4 h-4 accent-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2" />
-                        <label htmlFor="default-checkbox" className="ml-2 whitespace-nowrap text-sm font-medium text-gray-900">Random year?</label>
-                    </div>
-
+            <h3 className="pt-8 mb-5 text-xl font-bold text-gray-900">{props.title}</h3>
+            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+            <div className="relative flex flex-row gap-6 items-center justify-center">
+                <div className="flex flex-row items-center justify-left rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    <svg aria-hidden="true" className="ml-3 w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    <input value={textInput} disabled={isDisabled} onChange={(event) => {props.searchManager(props.title, event.target.value); setTextInput(event.target.value)}} type="number" id="default-search" className="focus:outline-none rounded-lg w-full block p-4 pl-4 text-sm text-gray-900" placeholder={props.placeholder} />                    
                 </div>
-            </form>
+                
+                <div className={"flex items-center flex-row " + checkboxVisible}>
+                    <input id="checkbox" onChange={(event) => {setIsDisabled(event.target.checked); setTextInput(''); props.searchManager(props.title, undefined)}} type="checkbox" value="" className="w-4 h-4 accent-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2" />
+                    <label htmlFor="default-checkbox" className="ml-2 whitespace-nowrap text-sm font-medium text-gray-900">Random year?</label>
+                </div>
+            </div>
         </div>
         );
 }
